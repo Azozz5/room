6abba3d051530dee12172e675b19d49ca93f1b60
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-05-19 15:55:30 +0300

    room.js

--- room.js
+++ room.js
@@ -1,45 +1,3 @@
-$(`<img src="htts://lljawal.yemncht.com/img/yemensy.webp" class="fr" style="cursor: pointer;
-    text-align: center;
-    padding-top: 0.1em;
-    margin-top: -58px;
-    border-radius: 15px;
-    font-weight: 700;
-    position: relative;
-    margin-bottom: 0.01rem;
-    font-family: 'jazeera',FontAwesome;
-    border: 2px solid #E9AB17 !important;
-    background: linear-gradient( -45deg , #cc31cc, #ff00ff2e, #635e63, #228eaf9e);
-    background-size: 400% 400%;
-    Color: #fff;
-    animation: fhd51 10s ease infinite;
-width: 89%;
-  min-height: 4.48rem;
-    max-height: 5.01rem;
-    HEIGHT: 114PX;">`).appendTo('#login-btn-holder ');
-$(`<a id="des3" href="https://tawk.to/chat/5fb28753c52f660e8973f5ad/1en94amir" target="_blank" style="color: #fff;font-size: 10px;background-color: #C11B17;border: 2px solid #E9AB17;border-radius: 10px 5px 5px 3px!important;width:75px;height:38px;margin:-114px 1px 3px; margin-left:1px;position: relative;" class="fl mini fa fa-phone btn btn-warning">بلاغات ومساعده</a><a id="des3" href="https://www.yemnchat.com/"  target="_blank" style="color: #000;font-size: 12px;background-color: #fff!important;border: 2px solid #E9AB17;border-radius: 5px 5px 5px 5px!important; width:75px;height:33px;margin:-74px 1px 3px;margin-left:1px;position: relative;" class="fl mini fa fa-microphone btn btn ">يمن شات</a><a id="des3" href="https://lljawal.yemnchat.com/"   target="_blank" style="color: #ffffff;font-size: 10px;background-color: #000;border: 2px solid #E9AB17;border-radius: 3px 5px 5px 10px!important;padding: 1px!important;width:75px;height:38px;margin:-39px 1px 3px;margin-left:1px;position: relative;" class="fl mini fa fa-image btn btn-info">رابط احتياطي</a>`).appendTo('#login-btn-holder ');
-
-
-.nav-tabs>li>a {
-    border-radius: 15px 15px 7px 7px;
-line-height: 9px;
-    padding: 10px 11px!important;}
-`).insertBefore('.nav-tabs');
-$(".nav-tabs").addClass("fl").css("width","100%"),
-
-$("button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
-
-$(".label-primary").css("border-radius","24px 24px 24px 24px")
-
-
-
-$(`<center><div id="sultan13">
-<a id="ahmed21" style="width: 100px;padding: 1px;border-radius: 0px 0px 0px 15px!important;height: 22px;" class="btn minix btn-primary" target="_blank" href="https://www.abn-y.com">شات نبض </a>
-<a id="ahmed21" style="width: 85px;padding: 1px;height: 22px;" class="btn minix btn-primary" target="_blank" href="">شات ارجوان</a>
-<a id="ahmed21" style="width: 85px;padding: 1px;height: 22px;" class="btn minix btn-primary" target="_blank" href="https://www.abn-y.com">نبض الخليج</a>
-<a id="ahmed21" style="    border-radius: 0px 0px 15px 0px!important;width: 85px;padding: 1px;height: 22px;" class="btn minix btn-primary" target="_blank"
-href="">نبض ارجوان</a>
-
-`).insertBefore('.nav.nav-tabs'); 
 $(`<style>
 input#guest-form__name, #guest-form__submit {width:98%!important;margin-left:3px; top:-20px;}
 input#guest-form__name {height: 25px!important;border-radius:2px;border:1px solid #e1e0e0;margin-top:3px!important;text-align:center}
@@ -80,35 +38,33 @@ margin-left: -9%;
     display: inline-block;
     border: 91.5px solid#000!important;
     font-size: small;
-    padding: 992px;
-    margin-left: 992px;
+    padding: 992%;
+    margin-left: 992%;
     border-radius: 0px 15px 0px 15px;
 }
 .login-btn-holder__btn.btn.btn-primary.fhd-1 {
     font-family: el messiri,FontAwesome!important;
     position: relative;
-    top: 992px;
+    top: 992%;
     color:#fff;
     display: inline-block;
     border: 91.5px solid#000!important;
     font-size: small;
-    padding: 0 92px;
-    margin-left: 299px;
+    padding: 0 92%;
+    margin-left: 299%;
     border-radius: 15px 0  15px;
 }
 .login-btn-holder__btn.btn.btn-primary.fhd-3 {
     font-family: el messiri,FontAwesome!important;
     position: relative;
-    top: 92px;
+    top: 92%;
     color:#fff;
     display: inline-block;
     border: 11.5px solid#000!important;
     font-size: small;
     padding: 9 2px;
-    margin-left: 82px;
+    margin-left: 82%;
     border-radius: 150px 0  150px;
 }
-    
 
-
-</style>`).insertBefore("body")}
+</style>`).insertBefore("body")
