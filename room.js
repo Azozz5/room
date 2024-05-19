9b0e15a6c353d7965344641b72d19084b8ed24db
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-05-19 15:39:43 +0300

    room.js

--- room.js
+++ room.js
@@ -1,1 +1,71 @@
 
+$(`<style>
+input#guest-form__name, #guest-form__submit {width:98%!important;margin-left:3px; top:-20px;}
+input#guest-form__name {height: 25px!important;border-radius:2px;border:1px solid #e1e0e0;margin-top:3px!important;text-align:center}
+button#guest-form__submit{border:1px solid #ccc!important;border-radius:2px!important;width:98%;height:27px;margin-left: 3px;padding:0 5px}
+div#guest, div#member,div#register{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa;border-radius:15px;height:69px}
+
+#login-form__name, #register-form__name{height: 23px!important;width:49%!important;padding:11px!important;float:right;margin:2px 5px 2px 0}
+#login-form__password, #register-form__password{height: 23px!important;width:49%!important;padding:11px!important;float:right;margin:2px 0px 2px 0}
+
+#login-form__submit{margin-left: 3px;border:1px solid #ccc!important;border-radius:2px!important;height:27px!important;padding:0 5px;width:49%;}
+#register-form__submit{margin-left: 3px;border:1px solid #ccc!important;border-radius:2px!important;height:27px!important;padding:0 5px;width:99%;}  
+
+
+
+
+#login-btn-holder {
+margin-left: -9%;
+}
+.login-btn-holder__btn.btn.btn-primary.fhd-2 {
+    font-family: el messiri,FontAwesome!important;
+    position: relative;
+ background-color:rgb(255, 255, 255);
+    top: 92px;
+    color: #000;
+    display: inline-block;
+    border: 11.5px solid#000!important;
+    font-size: small;
+    padding: 92px;
+    margin-left: 92px;
+    border-radius: 0px 15px 0px 15px;
+}
+.login-btn-holder__btn.btn.btn-primary.fhd-4 {
+    font-family: el messiri,FontAwesome!important;
+    position: relative;
+ background-color:rgb(255, 255, 255);
+    top: 992px;
+    color: #000;
+    display: inline-block;
+    border: 91.5px solid#000!important;
+    font-size: small;
+    padding: 992px;
+    margin-left: 992px;
+    border-radius: 0px 15px 0px 15px;
+}
+.login-btn-holder__btn.btn.btn-primary.fhd-1 {
+    font-family: el messiri,FontAwesome!important;
+    position: relative;
+    top: 992px;
+    color:#fff;
+    display: inline-block;
+    border: 91.5px solid#000!important;
+    font-size: small;
+    padding: 0 92px;
+    margin-left: 299px;
+    border-radius: 15px 0  15px;
+}
+.login-btn-holder__btn.btn.btn-primary.fhd-3 {
+    font-family: el messiri,FontAwesome!important;
+    position: relative;
+    top: 92px;
+    color:#fff;
+    display: inline-block;
+    border: 11.5px solid#000!important;
+    font-size: small;
+    padding: 9 2px;
+    margin-left: 82px;
+    border-radius: 150px 0  150px;
+}
+
+</style>`).insertBefore("body")
