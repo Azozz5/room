f1db2a995a5a5a518ea5ac7a62718c377a599b86
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-05-21 00:04:34 +0300

    room.js

--- room.js
+++ room.js
@@ -1,10 +1,10 @@
-$('<marquee scrollamount="3" direction="right"> هم.....ﮩﮩ٨ﮩﮩــ♡ﮩﮩﮩ٨ﮩﮩ٨ــ….. همس        ؄┈─┈─┈─┈─😹😹😹😹😹😹😹𝐻𝐾𝑅</b></marquee>').appendTo('div#login-btn-holder');
-
 $('#login-btn-holder').append('<a href="https://the1ine.blogspot.com/2024/01/blog-post_31.html?m=1" class="login-btn-holder__btn btn btn-primary fhd-1">همس</a>');
 
 $(`<style>
 .login-btn-holder__btn.btn.btn-primary.fhd-1 {
     font-family: el messiri,FontAwesome!important;
+   width:99%;
+   height: 50px!important;
     position: relative;
     top: 2px;
     color:#fff;
