65537666706d40b7a8c4c3ad615409a3c02e41cc
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-05-20 23:30:45 +0300

    room.js

--- room.js
+++ room.js
@@ -1,4 +1,4 @@
 $('<marquee scrollamount="3" direction="right"> هم.....ﮩﮩ٨ﮩﮩــ♡ﮩﮩﮩ٨ﮩﮩ٨ــ….. همس        ؄┈─┈─┈─┈─😹😹😹😹😹😹😹𝐻𝐾𝑅</b></marquee>').appendTo('div#login-btn-holder');
 
-$('#login-btn-holder').append('<a href="https://the-linee.blogspot.com/2023/12/blog-post_30.html?m=1" class="login-btn-holder__btn btn btn-primary fhd-1">اهمس</a>');
-$('#login-btn-holder').append('<a href="/" class="login-btn-holder__btn btn btn-primary fhd-2">😹😹</a>');
+$('#login-btn-holder').append('<a href="https://the-linee.blogspot.com/2023/12/blog-post_30.html?m=1" class="login-btn-holder__btn btn btn-primary fhd-1">همس</a>');
+$('#login-btn-holder').append('<a href="/" class="login-btn-holder__btn btn btn-primary fhd-2">شات..</a>');
