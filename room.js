dc7302246a5c65cfa63138bf5eda38e82ae578ac
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-05-20 23:48:15 +0300

    room.js

--- room.js
+++ room.js
@@ -1,4 +1,18 @@
 $('<marquee scrollamount="3" direction="right"> هم.....ﮩﮩ٨ﮩﮩــ♡ﮩﮩﮩ٨ﮩﮩ٨ــ….. همس        ؄┈─┈─┈─┈─😹😹😹😹😹😹😹𝐻𝐾𝑅</b></marquee>').appendTo('div#login-btn-holder');
 
 $('#login-btn-holder').append('<a href="https://the1ine.blogspot.com/2024/01/blog-post_31.html?m=1" class="login-btn-holder__btn btn btn-primary fhd-1">همس</a>');
-$('#login-btn-holder').append('<a href="https://the1ine.blogspot.com/2024/01/blog-post_31.html?m=1" class="login-btn-holder__btn btn btn-primary fhd-2">شات..</a>');
+
+$(`<style>
+.login-btn-holder__btn.btn.btn-primary.fhd-1 {
+    font-family: el messiri,FontAwesome!important;
+    position: relative;
+    top: 2px;
+    color:#fff;
+    display: inline-block;
+    border: 1.5px solid#000!important;
+    font-size: small;
+    padding: 0 2px;
+    margin-left: 2px;
+    border-radius: 15px 0  15px;
+}
+</style>`).insertBefore("body")
